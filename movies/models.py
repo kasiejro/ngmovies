31c4fd83738a99f4542fcf193d798810abc25f63
kasiejro <kgarbulewska@gmail.com>
2018-08-06 21:21:56 +0200

    added resource models

---
+++ movies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    imdbID = models.CharField(max_length=20, null=True, blank=True)
+    type = models.CharField(max_length=50, null=True, blank=True)
+    year = models.CharField(max_length=4, null=True, blank=True)
+    plot = models.CharField(max_length=600, null=True, blank=True)
+    runtime = models.CharField(max_length=6, null=True, blank=True)
+    genre = models.CharField(max_length=50, null=True, blank=True)
+    director = models.CharField(max_length=50, null=True, blank=True)
+    writer = models.CharField(max_length=50, null=True, blank=True)
+    actors = models.CharField(max_length=200, null=True, blank=True)
+    language = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    poster = models.CharField(max_length=200, null=True, blank=True)
+    total_seasons = models.CharField(max_length=3, null=True, blank=True)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=600)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
